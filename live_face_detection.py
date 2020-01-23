09ce598251c77b5b00095553d86875f5edd73447
Ankit Jayswal <ankitjayswal87@gmail.com>
2020-01-23 12:09:04 +0530

    Update live_face_detection.py

--- live_face_detection.py
+++ live_face_detection.py
@@ -3,6 +3,15 @@ import time
 import datetime
 import mysql.connector
 import uuid
+import face_recognition
+import os
+
+# sample known images of sachin and kohli
+known_images = ['Ankit.jpg','Kushal.jpg']
+
+# initial image encoding to compare with video frame
+cmp_picture = face_recognition.load_image_file("sachin.jpg")
+cmp_encoding = face_recognition.face_encodings(cmp_picture)[0]
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -32,19 +41,62 @@ while (cap.isOpened()):
 		roi_gray = gray_frame[y:y+h, x:x+w] # getting region of interest from gray frame, read by camera
 		roi_color = frame[y:y+h, x:x+w] # getting region of interest from color frame, read by camera
 		curr_time = str(datetime.datetime.now())
-		#cv2.imwrite('face_roi_'+curr_time+'.jpg', roi_color) # take snaps and save for detected faces
 
-		# geetting eye bounding rectangle values
+		# getting eye bounding rectangle values
 		eyes = eye_cascade.detectMultiScale(roi_gray)
 		for (ex,ey,ew,eh) in eyes:
-			cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,0,255), 2)
+			#cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,0,255), 2)
 			temp_img_name = str(uuid.uuid4())+".jpg"
-			cv2.imwrite(temp_img_name, roi_color)
 
-			sql = "INSERT INTO image (image_name,event_time) VALUES (%s,%s)"
-			val = (temp_img_name,curr_time)
-			mycursor.execute(sql, val)
-			mydb.commit()
+			rgb_frame = cv2.cvtColor(roi_color, cv2.COLOR_BGR2RGB)
+			check_img_name = str(uuid.uuid4())+".jpg"
+			cv2.imwrite(check_img_name, rgb_frame)
+
+			frame_picture = face_recognition.load_image_file(check_img_name)
+			frame_encoding = face_recognition.face_encodings(frame_picture)
+			os.system('rm '+check_img_name)
+			if len(frame_encoding) > 0:
+				frame_encoding = face_recognition.face_encodings(frame_picture)[0]
+				results = face_recognition.compare_faces([frame_encoding], cmp_encoding)
+				cmp_encoding = frame_encoding
+				if results[0] == True:
+					print("same face detected")
+				else:
+					print("new face detected")
+					cv2.imwrite(temp_img_name, roi_color)
+					detection_flag = 0
+
+					for img in known_images:
+						temp_img = face_recognition.load_image_file(img)
+						known_image_encoding = face_recognition.face_encodings(temp_img)[0]
+						results = face_recognition.compare_faces([known_image_encoding], frame_encoding)
+
+						if results[0] == True:
+							detection_flag = 1
+							face_identify = img.split('.')[0]
+							detected_output = "It's a picture of "+face_identify
+						else:
+							unknown_face_identify = "Unknown"
+							not_detected_output = "It is unknown picture!"
+
+					if detection_flag == 1:
+						print(detected_output)
+						sql = "INSERT INTO image (image_name,person_detected,event_time) VALUES (%s,%s,%s)"
+						val = (temp_img_name,face_identify,curr_time)
+						mycursor.execute(sql, val)
+						mydb.commit()
+					else:
+						print(not_detected_output)
+						sql = "INSERT INTO image (image_name,person_detected,event_time) VALUES (%s,%s,%s)"
+						val = (temp_img_name,unknown_face_identify,curr_time)
+						mycursor.execute(sql, val)
+						mydb.commit()
+
+					
+			else:
+				print("no face detected")
+
+
 	cv2.imshow('FaceDetect',frame)
 	k = cv2.waitKey(10)
 	if k==27:
